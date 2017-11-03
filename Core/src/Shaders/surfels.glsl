655b0aae935e8eb3c4f644b3d00329ace87940a3
Luke Sun <lukeqsun@shectc.com>
2017-11-03 15:16:39 +0800

    Change confidence calc to use new size (960x540); Change bootstrap current pose.

--- Core/src/Shaders/surfels.glsl
+++ Core/src/Shaders/surfels.glsl
@@ -2,16 +2,16 @@
  * This file is part of ElasticFusion.
  *
  * Copyright (C) 2015 Imperial College London
- * 
- * The use of the code within this file and all code within files that 
- * make up the software that is ElasticFusion is permitted for 
- * non-commercial purposes only.  The full terms and conditions that 
- * apply to the code within this file are detailed within the LICENSE.txt 
- * file and at <http://www.imperial.ac.uk/dyson-robotics-lab/downloads/elastic-fusion/elastic-fusion-license/> 
- * unless explicitly stated.  By downloading this file you agree to 
+ *
+ * The use of the code within this file and all code within files that
+ * make up the software that is ElasticFusion is permitted for
+ * non-commercial purposes only.  The full terms and conditions that
+ * apply to the code within this file are detailed within the LICENSE.txt
+ * file and at <http://www.imperial.ac.uk/dyson-robotics-lab/downloads/elastic-fusion/elastic-fusion-license/>
+ * unless explicitly stated.  By downloading this file you agree to
  * comply with these terms.
  *
- * If you wish to use any of this code for commercial purposes then 
+ * If you wish to use any of this code for commercial purposes then
  * please email researchcontracts.engineering@imperial.ac.uk.
  *
  */
@@ -19,9 +19,9 @@
 float getRadius(float depth, float norm_z)
 {
     float meanFocal = ((1.0 / abs(cam.z)) + (1.0 / abs(cam.w))) / 2.0;
-    
+
     const float sqrt2 = 1.41421356237f;
-    
+
     float radius = (depth / meanFocal) * sqrt2;
 
     float radius_n = radius;
@@ -35,12 +35,12 @@ float getRadius(float depth, float norm_z)
 
 float confidence(float x, float y, float weighting)
 {
-    const float maxRadDist = 400; //sqrt((width * 0.5)^2 + (height * 0.5)^2)
+    const float maxRadDist = 550; //sqrt((width * 0.5)^2 + (height * 0.5)^2)
     const float twoSigmaSquared = 0.72; //2*(0.6^2) from paper
-    
+
     vec2 pixelPosCentered = vec2(x, y) - cam.xy;
-    
+
     float radialDist = sqrt(dot(pixelPosCentered, pixelPosCentered)) / maxRadDist;
-    
+
     return exp((-(radialDist * radialDist) / twoSigmaSquared)) * weighting;
 }
